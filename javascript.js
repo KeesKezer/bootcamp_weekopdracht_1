e112a493d1ff9f73404c9645902a79aef7ca439b
KeesKezer <44520938+KeesKezer@users.noreply.github.com>
2018-11-10 09:29:23 +0100

    not final

---
+++ javascript.js
@@ -0,0 +1,22 @@
+ function swapStyleSheet(sheet) {
+    console.log(sheet);
+
+    document.getElementById("pagestyle").setAttribute("href", sheet);  
+}
+
+function initiate() {
+   
+    var style1 = document.getElementById("stylesheet1");
+
+    
+   
+}
+
+function initiate2() {
+   
+    var style2 = document.getElementById("stylesheet2");
+
+    
+   
+}
+
